4be9a75d2eda85f7d4bd8a11b55d1797b3f01506
Ma, Liangliang <liangliang.ma@intel.com>
2025-08-06 02:08:18 -0700

    add rope
    Signed-off-by: Ma, Liangliang <liangliang.ma@intel.com>

--- csrc/xpu/ops.h
+++ csrc/xpu/ops.h
@@ -7,3 +7,7 @@ void rms_norm(torch::Tensor &out, torch::Tensor &input, torch::Tensor &weight,
 
 void fused_add_rms_norm(torch::Tensor &input, torch::Tensor &residual,
                         torch::Tensor &weight, double epsilon);
+
+void rotary_embedding(torch::Tensor& positions, torch::Tensor& query,
+                      std::optional<torch::Tensor> key, int64_t head_size,
+                      torch::Tensor& cos_sin_cache, bool is_neox);
