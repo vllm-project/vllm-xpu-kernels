f344f406ce3369df1a77377ef32140b58157e14a
mayuyuace <qiming1.zhang@intel.com>
2025-08-17 21:53:00 -0700

    update benchmark

--- benchmark/benchmark_grouped_topk.py
+++ benchmark/benchmark_grouped_topk.py
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-import time
+import itertools
 from argparse import ArgumentParser
 from typing import Optional
 
 import torch
+import triton
 
 from tests.ops.grouped_topk import grouped_topk, grouped_topk_native
 
@@ -71,123 +72,136 @@ def grouped_topk_compile(
     return topk_weights.to(torch.float32), topk_ids.to(torch.int32)
 
 
-@torch.inference_mode()
-def main(
-    dtype: torch.dtype,
-    num_tokens: int,
-    num_experts: int,
-    topk: int,
-    renormalize: bool,
-    num_expert_group: int,
-    topk_group: int,
-    scoring_func: str = "softmax",
-    has_bias: bool = False,
-    seed: int = 0,
-    num_warmup_iters: int = 5,
-    num_iters: int = 100,
-    provider: str = "vllm",
-) -> None:
-    torch.manual_seed(seed)
-    torch.set_default_device("xpu")
+num_tokens_range = [1, 2, 64, 256]
+num_experts_range = [64, 128, 256]
+topk_range = [4, 6, 8]
+renormalize_range = [True, False]
+num_expert_group_range = [8]
+topk_group_range = [4, 6, 8]
+scoring_func_range = ["sigmoid", "softmax"]
+has_bias_range = [True, False]
+configs = list(
+    itertools.product(
+        num_tokens_range,
+        num_experts_range,
+        topk_range,
+        renormalize_range,
+        num_expert_group_range,
+        topk_group_range,
+        scoring_func_range,
+        has_bias_range,
+    ))
 
-    gating_output = torch.randn(num_tokens, num_experts,
-                                device=dpcpp_device).to(dtype)
-    hidden_states = torch.zeros(num_tokens, num_experts,
-                                device=dpcpp_device).to(dtype)
-    bias = None
-    if has_bias:
-        if has_bias and scoring_func == "sigmoid" \
-            and dtype is not torch.float32:
-            # using a bias of bigger number to avoid Low-precision
-            bias = torch.arange(1, num_experts + 1).to(dpcpp_device).to(dtype)
-        else:
-            bias = torch.randn(num_experts, device=dpcpp_device).to(dtype)
 
-    def run_xpu_benchmark(num_iters: int) -> float:
-        torch.xpu.synchronize()
+def get_benchmark():
 
-        start_time = time.perf_counter()
+    @triton.testing.perf_report(
+        triton.testing.Benchmark(
+            x_names=[
+                "num_tokens", "num_experts", "topk", "renormalize",
+                "num_expert_group", "topk_group", "scoring_func", "has_bias"
+            ],
+            x_vals=[tuple(_) for _ in configs],
+            line_arg="provider",
+            line_vals=["vllm", "native", "compile"],
+            line_names=["vllm", "native", "compile"],
+            styles=[("blue", "-"), ("green", "-"), ("orange", "-"),
+                    ("red", "-")],
+            ylabel="us",
+            plot_name="grouped_topk-perf",
+            args={},
+        ))
+    def benchmark(
+        num_tokens: int,
+        num_experts: int,
+        topk: int,
+        renormalize: bool,
+        num_expert_group: int,
+        topk_group: int,
+        scoring_func: str = "softmax",
+        has_bias: bool = False,
+        provider: str = "vllm",
+    ):
+        dtype = torch.float16
+        torch.set_default_device("xpu")
+
+        gating_output = torch.randn(num_tokens,
+                                    num_experts,
+                                    device=dpcpp_device).to(dtype)
+        hidden_states = torch.zeros(num_tokens,
+                                    num_experts,
+                                    device=dpcpp_device).to(dtype)
+
+        bias = None
+        if has_bias:
+            if has_bias and scoring_func == "sigmoid" \
+                and dtype is not torch.float32:
+                # using a bias of bigger number to avoid Low-precision
+                bias = torch.arange(1,
+                                    num_experts + 1).to(dpcpp_device).to(dtype)
+            else:
+                bias = torch.randn(num_experts, device=dpcpp_device).to(dtype)
+
+        quantiles = [0.5, 0.2, 0.8]
 
         if provider == "vllm":
-            run_op = grouped_topk
-        elif provider == "native":
-            run_op = grouped_topk_native
-        elif provider == "compile":
-            run_op = grouped_topk_compile
-        else:
-            raise ValueError(f"Unsupported provider: {provider}")
-        for _ in range(num_iters):
-            topk_weights, topk_indices = run_op(
-                hidden_states,
-                gating_output,
-                topk,
-                renormalize,
-                num_expert_group,
-                topk_group,
-                scoring_func=scoring_func,
-                e_score_correction_bias=bias,
+            ms, min_ms, max_ms = triton.testing.do_bench(
+                lambda: grouped_topk(
+                    hidden_states,
+                    gating_output,
+                    topk,
+                    renormalize,
+                    num_expert_group,
+                    topk_group,
+                    scoring_func=scoring_func,
+                    e_score_correction_bias=bias,
+                ),
+                quantiles=quantiles,
             )
-        torch.xpu.synchronize()
+        elif provider == "native":
+            ms, min_ms, max_ms = triton.testing.do_bench(
+                lambda: grouped_topk_native(
+                    hidden_states,
+                    gating_output,
+                    topk,
+                    renormalize,
+                    num_expert_group,
+                    topk_group,
+                    scoring_func=scoring_func,
+                    e_score_correction_bias=bias,
+                ),
+                quantiles=quantiles,
+            )
+        else:
+            ms, min_ms, max_ms = triton.testing.do_bench(
+                lambda: grouped_topk_compile(
+                    hidden_states,
+                    gating_output,
+                    topk,
+                    renormalize,
+                    num_expert_group,
+                    topk_group,
+                    scoring_func=scoring_func,
+                    e_score_correction_bias=bias,
+                ),
+                quantiles=quantiles,
+            )
 
-        end_time = time.perf_counter()
+        return 1000 * ms, 1000 * max_ms, 1000 * min_ms
 
-        return (end_time - start_time) / num_iters
-
-    # Warmup.
-    print("Warming up...")
-    run_benchmark = run_xpu_benchmark
-    run_benchmark(num_iters=num_warmup_iters)
-
-    # Benchmark.
-    latency = run_benchmark(num_iters=num_iters)
-    print(f"Kernel running time: {latency * 1000000:.3f} us")
+    return benchmark
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Benchmark the grouped topk kernel.")
-    parser.add_argument("--num-tokens", type=int, default=64)
-    parser.add_argument("--num-experts", type=int, default=128)
-    parser.add_argument("--topk", type=int, default=6)
-    parser.add_argument("--renormalize", action="store_true")
-    parser.add_argument("--num-expert-group", type=int, default=8)
-    parser.add_argument("--topk-group", type=int, default=8)
-    parser.add_argument("--scoring-func",
-                        type=str,
-                        choices=["sigmoid", "softmax"],
-                        default="softmax")
-    parser.add_argument("--has-bias", action="store_true")
-    parser.add_argument("--dtype",
-                        type=str,
-                        choices=["half", "bfloat16", "float"],
-                        default="half")
-    parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--num-warmup-iters", type=int, default=5)
-    parser.add_argument("--num-iters",
-                        type=int,
-                        default=100,
-                        help="Number of benchmark iterations. ")
-    parser.add_argument("--provider",
-                        type=str,
-                        choices=["vllm", "native", "compile"],
-                        default="vllm")
+    parser.add_argument(
+        "--save-path",
+        type=str,
+        default="./configs/grouped_topk/",
+        help="Path to save grouped_topk benchmark results",
+    )
 
     args = parser.parse_args()
-    print(args)
 
-    # Convert dtype string to torch.dtype
-    dtype = getattr(torch, args.dtype)
-    main(
-        dtype=dtype,
-        num_tokens=args.num_tokens,
-        num_experts=args.num_experts,
-        topk=args.topk,
-        renormalize=args.renormalize,
-        num_expert_group=args.num_expert_group,
-        topk_group=args.topk_group,
-        scoring_func=args.scoring_func,
-        has_bias=args.has_bias,
-        seed=args.seed,
-        num_warmup_iters=args.num_warmup_iters,
-        num_iters=args.num_iters,
-        provider=args.provider,
-    )
+    benchmark = get_benchmark()
+    benchmark.run(print_data=True, save_path=args.save_path)
