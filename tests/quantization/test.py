d7af2516671b40fe302e56a4306bf593388511f1
Ma, Liangliang <liangliang.ma@intel.com>
2025-08-25 01:02:20 -0700

    add base gemm test
    Signed-off-by: Ma, Liangliang <liangliang.ma@intel.com>

--- tests/quantization/test.py
+++ tests/quantization/test.py
@@ -1,6 +1,31 @@
 import pytest
 import torch
 
-from vllm_xpu_kernels.scaled_mm_interface import cutlass_scaled_mm
+from vllm_xpu_kernels.scaled_mm_interface import cutlass_basic_gemm, cutlass_scaled_mm
 
+DTYPES = [torch.bfloat16]
 
+# @pytest.mark.parametrize("dtype", DTYPES)
+def test_base_gemm(dtype):
+    # import ipdb
+    # ipdb.set_trace()
+    torch.set_default_device("xpu")
+
+    m = 4096
+    n = 4096
+    k = 4096
+
+    inputA = torch.randn(m, k, dtype=dtype)
+    inputB = torch.randn(k, n, dtype=dtype)
+    inputC = torch.randn(m, n, dtype=dtype)
+    res = torch.empty_like(inputC)
+
+    alpha = 1.0
+    beta = 0.1
+    ref_D = alpha * (inputA @ inputB) + beta * inputC
+    cutlass_basic_gemm(inputA, inputB, inputC, res, alpha, beta)
+    print(res)
+    print(ref_D)
+
+if __name__ == '__main__':
+    test_base_gemm(DTYPES[0])
