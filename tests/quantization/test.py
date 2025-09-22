79037711997486606be4bc3e66aa0c417f4925a6
Ma, Liangliang <liangliang.ma@intel.com>
2025-09-22 08:22:38 +0000

    add profile
    Signed-off-by: Ma, Liangliang <liangliang.ma@intel.com>

--- tests/quantization/test.py
+++ tests/quantization/test.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+import torch.profiler
 from math import ceil
 
 from vllm_xpu_kernels.scaled_mm_interface import cutlass_basic_gemm, cutlass_scaled_mm
@@ -56,8 +57,8 @@ def ref_scaled_mm(inputA_fp8, inputB_fp8, scaleA, scaleB, block_size):
     a_full = a_scale.half()
     b_full = b_scale.half()
 
-    A32 = inputA_fp8.to(torch.float32)
-    B32 = inputB_fp8.to(torch.float32)
+    A32 = inputA_fp8.to(torch.float16)
+    B32 = inputB_fp8.to(torch.float16)
 
     res = (A32 * a_full) @ (B32 * b_full).T
     return res
@@ -66,11 +67,11 @@ def ref_scaled_mm(inputA_fp8, inputB_fp8, scaleA, scaleB, block_size):
 def test_scaled_mm():
     torch.set_default_device("xpu")
 
-    m = 1024
-    n = 2048
+    m = 4096
+    n = 4096
     k = 4096
     block_n, block_k = (16, 16)
-    
+
     dtype = torch.float8_e4m3fn
 
     inputA = torch.randn(m, k, dtype=torch.float32).to(dtype)
@@ -84,10 +85,68 @@ def test_scaled_mm():
     scaleB = torch.rand((scale_n, scale_k), dtype=torch.float16)
 
     out = cutlass_scaled_mm(inputA, inputB, scaleA, scaleB, [block_n, block_k])
-    
+    ref = ref_scaled_mm(inputA, inputB, scaleA, scaleB, [block_n, block_k])
     print(out)
-    print(ref_scaled_mm(inputA, inputB, scaleA, scaleB, [block_n, block_k]))
+    print(ref)
+
+    try:
+        torch.testing.assert_close(out.float(), ref.float(), rtol=1e-2, atol=1e-2)
+        print("a and b are close enough")
+    except AssertionError as e:
+        print("a and b are different")
+        print(e)
+
+def profile_scaled_mm():
+    torch.set_default_device("xpu")
+
+    m = 4096
+    n = 4096
+    k = 4096
+    block_n, block_k = (16, 16)
+
+    dtype = torch.float8_e4m3fn
+
+    inputA = torch.randn(m, k, dtype=torch.float32).to(dtype)
+    inputB = torch.randn(n, k, dtype=torch.float32).to(dtype)
+    inputC = torch.randn(m, n, dtype=torch.float32)
+    res = torch.empty_like(inputC)
+
+    scale_k = ceil(k / block_k)
+    scaleA = torch.rand((m, scale_k), dtype=torch.float16)
+    scale_n = ceil(n / block_n)
+    scaleB = torch.rand((scale_n, scale_k), dtype=torch.float16)
+
+    for _ in range(5):
+        out = cutlass_scaled_mm(inputA, inputB, scaleA, scaleB, [block_n, block_k])
+
+    with torch.profiler.profile(
+        activities=[
+            # torch.profiler.ProfilerActivity.CPU,
+            torch.profiler.ProfilerActivity.XPU
+        ],
+        # record_shapes=True,
+        with_stack=False,
+        # profile_memory=True,
+        with_flops=True,
+     ) as prof:
+        out = cutlass_scaled_mm(inputA, inputB, scaleA, scaleB, [block_n, block_k])
+
+    print(prof.key_averages().table(
+         sort_by="self_xpu_time_total",
+         row_limit=-1
+     ))
+
+    flops = 0
+    flops += 2*m*n*k #mm
+    flops += m*k + n*k #dequante
+    flops /= 1e9
+    mem = 0
+    mem += m*k*1 + n*k*1 + m*k*2 + n*k*2 + m*n*4
+    print(f"FLOPs: {flops:.3f} G")
+    print(f"Memory: {mem / 1e6:.3f} MB")
+
 
 if __name__ == '__main__':
     # test_base_gemm(DTYPES[0])
-    test_scaled_mm()
+    # test_scaled_mm()
+    profile_scaled_mm()
