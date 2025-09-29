c83161ac8949d2568b14cc4fb0a9c9810186ed71
sanchitintel <sanchit.jain@intel.com>
2025-09-29 00:20:25 -0700

    Change TiledMma

--- csrc/xpu/cutlass_kernels/grouped_gemm_kernel.cpp
+++ csrc/xpu/cutlass_kernels/grouped_gemm_kernel.cpp
@@ -232,10 +232,8 @@ void kernel_functor(sycl::queue& stream, void* ptr_A, void* ptr_B, void* ptr_D,
   using GmemTiledCopyB = XE_2D_U16x16x16_LD_T;
   // This TiledMMA is the default one in intel/cutlass-sycl examples
   using TiledMma =
-      TiledMMA<MMA_Atom<XE_8x16x16_F32BF16BF16F32_TT>,
-               Layout<Shape<_8, _4, _1>, Stride<_4, _1, _0>>,
-               Tile<Layout<Shape<_8, _8, _4>, Stride<_1, _32, _8>>,
-                    Layout<Shape<_16, _4, _4>, Stride<_1, _64, _16>>, _32>>;
+      typename TiledMMAHelper<MMA_Atom<XE_8x16x16_F32BF16BF16F32_TT>, Layout<TileShape>,
+                                    Layout<Shape<_8, _4, _1>, Stride<_4, _1, _0>>>::TiledMMA;
 
   constexpr int PipelineStages = 2;
   using GEMMDispatchPolicy =
