2106c1645f973524a33d32621eea22da01bc739c
Kunshang Ji <kunshang.ji@intel.com>
2026-01-21 16:55:26 -0800

    apply to paged
    Signed-off-by: Kunshang Ji <kunshang.ji@intel.com>

--- csrc/xpu/attn/xe_2/paged_decode.hpp
+++ csrc/xpu/attn/xe_2/paged_decode.hpp
@@ -292,6 +292,9 @@ template <
     typename SubgroupLayoutQK,
     typename SubgroupLayoutPV_, /* void -> default */
     int PipelineStages,
+    bool Causal = false,
+    bool Local = false,
+    bool Sink = false,
     typename ElementQ = bfloat16_t,
     typename ElementK = bfloat16_t,
     typename ElementV = bfloat16_t,
@@ -318,7 +321,7 @@ struct PagedDecodeConfig {
       decltype(cutlass::fmha::collective::get_sg_layout_pv(SubgroupLayoutQK{})),
       SubgroupLayoutPV_>;
 
-  template <class Scheduler, bool Causal, bool Local, bool Sink>
+  template <class Scheduler>
   static void run(sycl::queue& queue, const paged_decode_args_t& args) {
     constexpr bool VarLen = true;
     constexpr bool Paged = true;
@@ -396,24 +399,14 @@ struct PagedDecodeConfig {
     launcher.run(queue, args, hw_info);
   }
 
-  template <bool... Bs>
   static void
   kernel_dispatch(sycl::queue& queue, const paged_decode_args_t& args) {
-    return run<cutlass::fmha::kernel::DecodeTileScheduler, Bs...>(queue, args);
-  }
-
-  template <bool... Bs, typename... Ts>
-  static void kernel_dispatch(
-      sycl::queue& queue, const paged_decode_args_t& args, bool b, Ts... ts) {
-    if (b) {
-      kernel_dispatch<Bs..., true>(queue, args, ts...);
-    } else {
-      kernel_dispatch<Bs..., false>(queue, args, ts...);
-    }
+    return run<cutlass::fmha::kernel::DecodeTileScheduler>(queue, args);
   }
 };
 
-template <typename decode_policy>
+// Template function for explicit instantiation
+template <typename decode_policy, bool Causal, bool Local, bool Sink>
 void decode_policy_dispatch(
     sycl::queue& queue, CutlassType cuType, const paged_decode_args_t& args) {
   const int PipelineStages = 1;
@@ -425,12 +418,13 @@ void decode_policy_dispatch(
         typename decode_policy::SubgroupLayoutQK,
         void,
         PipelineStages,
+        Causal,
+        Local,
+        Sink,
         half_t,
         half_t,
         half_t,
-        half_t>::
-        kernel_dispatch(
-            queue, args, args.is_causal, args.is_local, args.is_sink);
+        half_t>::kernel_dispatch(queue, args);
   } else {
     return PagedDecodeConfig<
         typename decode_policy::ShapeQK,
@@ -439,12 +433,36 @@ void decode_policy_dispatch(
         typename decode_policy::SubgroupLayoutQK,
         void,
         PipelineStages,
+        Causal,
+        Local,
+        Sink,
         bfloat16_t,
         bfloat16_t,
         bfloat16_t,
-        bfloat16_t>::
-        kernel_dispatch(
-            queue, args, args.is_causal, args.is_local, args.is_sink);
+        bfloat16_t>::kernel_dispatch(queue, args);
+  }
+}
+
+// Runtime dispatcher helper
+template <typename decode_policy, bool... Bs>
+void decode_policy_dispatch_impl(
+    sycl::queue& queue, CutlassType cuType, const paged_decode_args_t& args) {
+  decode_policy_dispatch<decode_policy, Bs...>(queue, cuType, args);
+}
+
+template <typename decode_policy, bool... Bs, typename... Ts>
+void decode_policy_dispatch_impl(
+    sycl::queue& queue,
+    CutlassType cuType,
+    const paged_decode_args_t& args,
+    bool b,
+    Ts... ts) {
+  if (b) {
+    decode_policy_dispatch_impl<decode_policy, Bs..., true>(
+        queue, cuType, args, ts...);
+  } else {
+    decode_policy_dispatch_impl<decode_policy, Bs..., false>(
+        queue, cuType, args, ts...);
   }
 }
 
@@ -456,24 +474,24 @@ inline void dispatch_by_head_size(
     const paged_decode_args_t& args) {
   switch (head_case) {
     case 0:
-      decode_policy_dispatch<decode_policy_qpacked_head<QGroup, _64>>(
-          queue, cuType, args);
+      decode_policy_dispatch_impl<decode_policy_qpacked_head<QGroup, _64>>(
+          queue, cuType, args, args.is_causal, args.is_local, args.is_sink);
       break;
     case 1:
-      decode_policy_dispatch<decode_policy_qpacked_head<QGroup, _96>>(
-          queue, cuType, args);
+      decode_policy_dispatch_impl<decode_policy_qpacked_head<QGroup, _96>>(
+          queue, cuType, args, args.is_causal, args.is_local, args.is_sink);
       break;
     case 2:
-      decode_policy_dispatch<decode_policy_qpacked_head<QGroup, _128>>(
-          queue, cuType, args);
+      decode_policy_dispatch_impl<decode_policy_qpacked_head<QGroup, _128>>(
+          queue, cuType, args, args.is_causal, args.is_local, args.is_sink);
       break;
     case 3:
-      decode_policy_dispatch<decode_policy_qpacked_head<QGroup, _192>>(
-          queue, cuType, args);
+      decode_policy_dispatch_impl<decode_policy_qpacked_head<QGroup, _192>>(
+          queue, cuType, args, args.is_causal, args.is_local, args.is_sink);
       break;
     case 4:
-      decode_policy_dispatch<decode_policy_qpacked_head<QGroup, _256>>(
-          queue, cuType, args);
+      decode_policy_dispatch_impl<decode_policy_qpacked_head<QGroup, _256>>(
+          queue, cuType, args, args.is_causal, args.is_local, args.is_sink);
       break;
     default:
       TORCH_CHECK(false, "Unsupported head size for fmha");
@@ -504,102 +522,4 @@ void cutlass_paged_decode_impl(
     bool is_causal,
     bool is_local,
     bool is_sink,
-    int num_kv_splits) {
-  // general params
-  int batch_size, num_heads_q, num_heads_kv, head_size;
-  // additional params
-  int total_seqlen_q, total_seqlen_k;
-  int num_blocks, block_size, max_blocks_per_seq;
-  if (is_varlen) {
-    // query: [total_seq, num_heads, head_size]
-    batch_size = cu_seqlens_q.numel() - 1;
-    num_heads_q = query.size(1);
-    num_heads_kv = key_cache.size(1);
-    head_size = query.size(2);
-    total_seqlen_q = query.size(0);
-    total_seqlen_k = key_cache.size(0);
-  } else {
-    // query: [batch, num_heads, seq, head_size]
-    batch_size = query.size(0);
-    num_heads_q = query.size(1);
-    num_heads_kv = key_cache.size(1);
-    head_size = query.size(3);
-    max_seqlen_q = query.size(2);
-    max_seqlen_k = key_cache.size(2);
-  }
-  if (is_paged) {
-    // num_blocks is used to build total_seqlen_k for shape_K in kernels
-    // it is not just the meaning of used blocks for kv.
-    num_blocks = key_cache.size(0);
-    block_size = key_cache.size(1);
-    num_heads_kv = key_cache.size(2);
-    max_blocks_per_seq = block_table.size(1);
-    total_seqlen_k = num_blocks * block_size;
-  }
-
-  if (is_local) {
-    window_size_left = window_size_left == -1 ? max_seqlen_k : window_size_left;
-    window_size_right =
-        window_size_right == -1 ? max_seqlen_k : window_size_right;
-  }
-
-  paged_decode_args_t args = {
-      query.data_ptr(),
-      key_cache.data_ptr(),
-      value_cache.data_ptr(),
-      out.data_ptr(),
-      temp_out.data_ptr(),
-      exp_sums.data_ptr(),
-      max_logits.data_ptr(),
-      block_table.data_ptr(),
-      cu_seqlens_q.data_ptr(),
-      cu_seqlens_k.data_ptr(),
-      max_seqlen_q,
-      max_seqlen_k,
-      total_seqlen_q,
-      total_seqlen_k,
-      static_cast<float>(sm_scale),
-      is_sink ? sm_sink_.value().data_ptr() : nullptr,
-      batch_size,
-      num_heads_q,
-      num_heads_kv,
-      head_size,
-      max_blocks_per_seq,
-      block_size,
-      window_size_left,
-      window_size_right,
-      is_varlen,  // varlen
-      is_paged,   // paged
-      is_causal,
-      is_local,
-      is_sink,
-      num_kv_splits};
-
-  CutlassType cuType = aten_to_Cutlass_dtype(query);
-
-  static constexpr int max_head_size = 256;
-  TORCH_CHECK(
-      head_size <= max_head_size,
-      "FMHA forward only supports head dimension at most " +
-          std::to_string(max_head_size));
-
-  auto get_head_size_case = [](int head_size) -> int {
-    if (head_size <= HEAD_SIZE_LIMIT_0) return 0;
-    if (head_size <= HEAD_SIZE_LIMIT_1) return 1;
-    if (head_size <= HEAD_SIZE_LIMIT_2) return 2;
-    if (head_size <= HEAD_SIZE_LIMIT_3) return 3;
-    if (head_size <= HEAD_SIZE_LIMIT_4) return 4;
-    return -1;
-  };
-
-  int head_case = get_head_size_case(args.head_size);
-  int num_q_group_size = num_heads_q / num_heads_kv;
-
-  if (num_q_group_size <= 8) {
-    dispatch_by_head_size<_8>(head_case, queue, cuType, args);
-  } else if (num_q_group_size <= 16) {
-    dispatch_by_head_size<_16>(head_case, queue, cuType, args);
-  } else {
-    TORCH_CHECK(false, "Unsupported num_heads_q / num_heads_kv for fmha");
-  }
-}
+    int num_kv_splits);
