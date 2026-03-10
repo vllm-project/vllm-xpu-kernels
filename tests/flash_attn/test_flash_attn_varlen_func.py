953e5ad20b8b3cb685d885a5ce834553fc2d58f6
baodii <di.bao@intel.com>
2026-03-09 23:18:31 -0700

    skip UTs which make out of memory
    Signed-off-by: baodii <di.bao@intel.com>

--- tests/flash_attn/test_flash_attn_varlen_func.py
+++ tests/flash_attn/test_flash_attn_varlen_func.py
@@ -359,6 +359,8 @@ def test_decode_with_paged_kv(
     #                 "supported on version 3 with bfloat16 base type")
     if num_heads == (16, 1) and head_size == 256:
         pytest.skip("skip test cases that may run out of SLM.")
+    if block_size == 128 and num_blocks == 32768 and head_size >= 192:
+        pytest.skip("skip test cases that may run out of Memory.")
     if is_sink and window_size != (-1, -1):
         pytest.skip("sink not supported with sliding window")
     torch.manual_seed(42)
