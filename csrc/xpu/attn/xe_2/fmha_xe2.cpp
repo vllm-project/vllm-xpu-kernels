5e17aebf2010873cc5b74298caabc3dcde61c565
Yizhou Wang <yizhou.wang@intel.com>
2026-03-18 00:11:16 -0700

    new mem_format: [num_heads, seq, head_size]
    Signed-off-by: Yizhou Wang <yizhou.wang@intel.com>

--- csrc/xpu/attn/xe_2/fmha_xe2.cpp
+++ csrc/xpu/attn/xe_2/fmha_xe2.cpp
@@ -80,10 +80,12 @@ void cutlass_chunk_prefill_impl(
   if (is_varlen) {
     // query: [total_seq, num_heads, head_size]
     batch_size = cu_seqlens_q.numel() - 1;
-    num_heads_q = query.size(1);
+    // num_heads_q = query.size(1);
+    num_heads_q = query.size(0);
     num_heads_kv = key_cache.size(1);
     head_size = query.size(2);
-    total_seqlen_q = query.size(0);
+    // total_seqlen_q = query.size(0);
+    total_seqlen_q = query.size(1);
     total_seqlen_k = key_cache.size(0);
   } else {
     // query: [batch, num_heads, seq, head_size]
@@ -95,9 +97,13 @@ void cutlass_chunk_prefill_impl(
     max_seqlen_k = is_paged ? max_seqlen_q : key_cache.size(2);
   }
   if (is_paged) {
-    num_blocks = key_cache.size(0);
-    block_size = key_cache.size(1);
-    num_heads_kv = key_cache.size(2);
+    // key_cache: [num_heads, num_block, block_size, head_size]
+    // num_blocks = key_cache.size(0);
+    // block_size = key_cache.size(1);
+    // num_heads_kv = key_cache.size(2);
+    num_blocks = key_cache.size(1);
+    block_size = key_cache.size(2);
+    num_heads_kv = key_cache.size(0);
     max_blocks_per_seq = block_table.size(1);
     total_seqlen_k = num_blocks * block_size;
   }
--- csrc/xpu/attn/xe_2/kernel/chunk_prefill_kernel.hpp
+++ csrc/xpu/attn/xe_2/kernel/chunk_prefill_kernel.hpp
@@ -286,14 +286,16 @@ class XeFMHAFwdKernel {
         int group_heads_q = s.num_heads_q / s.num_heads_kv;
         auto qo_cumulative = s.seq_len_qo.cumulative_length;
         auto kv_cumulative = s.seq_len_kv.cumulative_length;
-        offset_q = s.num_heads_q * s.head_size_qk * qo_cumulative[idx_b];
+        // Offset = seq_stride * cumulative_pos.  seq_stride = get<0>(dQ/dO)
+        // for Q/O (seq is dim 0), get<1>(dV) for V (seq is dim 1).
+        offset_q = get<0>(p.dQ) * qo_cumulative[idx_b];
         offset_k = PagedKV
                        ? 0
-                       : s.num_heads_kv * s.head_size_qk * kv_cumulative[idx_b];
+                       : get<0>(p.dK) * kv_cumulative[idx_b];
         offset_v = PagedKV
                        ? 0
-                       : s.num_heads_kv * s.head_size_vo * kv_cumulative[idx_b];
-        offset_o = s.num_heads_q * s.head_size_vo * qo_cumulative[idx_b];
+                       : get<1>(p.dV) * kv_cumulative[idx_b];
+        offset_o = get<0>(p.dO) * qo_cumulative[idx_b];
       }
 
       auto batch_dim_qo = is_var_len ? 1 : s.batch;
@@ -315,16 +317,16 @@ class XeFMHAFwdKernel {
       auto dcO = const_cast<ElementO*>(p.O + offset_o);
 
       auto layout_q = is_var_len
-                          ? make_ordered_layout(shape_Q, Step<_2, _0, _1, _3>{})
+                          ? make_layout(shape_Q, p.dQ)
                           : make_layout(shape_Q, p.dQ);
       auto layout_k = (PagedKV || is_var_len)
-                          ? make_ordered_layout(shape_K, Step<_2, _0, _1, _3>{})
+                          ? make_layout(shape_K, p.dK)
                           : make_layout(shape_K, p.dK);
       auto layout_v = (PagedKV || is_var_len)
-                          ? make_ordered_layout(shape_V, Step<_0, _2, _1, _3>{})
+                          ? make_layout(shape_V, p.dV)
                           : make_layout(shape_V, p.dV);
       auto layout_o = is_var_len
-                          ? make_ordered_layout(shape_O, Step<_2, _0, _1, _3>{})
+                          ? make_layout(shape_O, p.dO)
                           : make_layout(shape_O, p.dO);
 
       Tensor Q = make_tensor(make_gmem_ptr(dcQ), layout_q);
