61657f3ccc3eb6cb8309efa1760ce412fd483fa3
mayuyuace <qiming1.zhang@intel.com>
2026-03-31 09:38:47 +0000

    opt remap
    Signed-off-by: mayuyuace <qiming1.zhang@intel.com>

--- csrc/moe/remap_hidden_states.cpp
+++ csrc/moe/remap_hidden_states.cpp
@@ -163,102 +163,98 @@ class RemapHiddenStates {
   static constexpr int GroupWorkItem = 256;
   static constexpr int WARP_SIZE = 32;
   static constexpr int ElemsPerItem = 4;
+  static constexpr int TopK = 8;
 
   static inline sycl::nd_range<1> get_nd_range(
       const int num_rows,
       const int n_experts_per_token,
       const int hidden_size) {
-    int group_nums = num_rows * n_experts_per_token;
-    int local_nums = GroupWorkItem;
-    if (GroupWorkItem * ElemsPerItem > hidden_size) {
-      local_nums = (hidden_size + ElemsPerItem - 1) / ElemsPerItem;
-    }
-    sycl::range<1> local(local_nums);
-    sycl::range<1> group(group_nums);
+    sycl::range<1> local(GroupWorkItem);
+    sycl::range<1> group(num_rows);
     return sycl::nd_range<1>(local * group, local);
   }
 
   void operator()
       [[sycl::reqd_sub_group_size(WARP_SIZE)]] (sycl::nd_item<1> item) const {
     auto local_id = item.get_local_id(0);
-    auto local_range = item.get_local_range(0);
     auto group_id = item.get_group(0);
 
-    int row = group_id / n_experts_per_token;
-    int topi = group_id % n_experts_per_token;
+    int row = group_id;
+    int global_expert_id[TopK];
+    int local_expert_id[TopK];
 
-    int global_expert_id = topk_ids[row * n_experts_per_token + topi];
+#pragma unroll
+    for(int i = 0; i < TopK; ++i){
+      global_expert_id[i] = topk_ids[row * TopK + i];
+    }
+#pragma unroll
+    for(int i = 0; i < TopK; ++i){
+      local_expert_id[i] = expert_map[global_expert_id[i]];
+    }
 
-    int local_expert_id = expert_map[global_expert_id];
-
-    if (local_expert_id == -1) {
-      // selected expert is not at current rank
-      if (local_id == 0) {
-        unpermuted_row_to_permuted_row[row * n_experts_per_token + topi] = -1;
+    int rows_offset[TopK];
+#pragma unroll
+    for(int i = 0; i < TopK; ++i){
+      if(local_expert_id[i] != -1){
+        rows_offset[i] = unpermuted_row_to_permuted_row[row * TopK + i] + expert_first_token_offset[local_expert_id[i]];
+      } else {
+        rows_offset[i] = -1;
       }
-      return;
     }
 
-    int rows_offset =
-        unpermuted_row_to_permuted_row[row * n_experts_per_token + topi];
-    int first_token_offset = expert_first_token_offset[local_expert_id];
-    rows_offset += first_token_offset;
-
-    item.barrier(sycl::access::fence_space::local_space);
-
-    if (local_id == 0) {
-      unpermuted_row_to_permuted_row[row * n_experts_per_token + topi] =
-          rows_offset;
-    }
-
-    int loop_count = (hidden_size + local_range * ElemsPerItem - 1) /
-                     (local_range * ElemsPerItem);
+    int stride = GroupWorkItem * ElemsPerItem;
+    int loop_count = (hidden_size + stride - 1) / stride;
 
     for (int l = 0; l < loop_count; ++l) {
-      int start_id = l * local_range * ElemsPerItem + local_id * ElemsPerItem;
-      int remained_elems = hidden_size - start_id;
-      if (remained_elems >= ElemsPerItem) {
-#pragma unroll
-        for (int i = 0; i < ElemsPerItem; ++i) {
-          int idx = start_id + i;
-          remapped_hidden_states[rows_offset * hidden_size + idx] =
-              hidden_states[row * hidden_size + idx];
-        }
-      } else if (remained_elems > 0) {
-#pragma unroll
-        for (int i = 0; i < remained_elems; ++i) {
-          int idx = start_id + i;
-          remapped_hidden_states[rows_offset * hidden_size + idx] =
-              hidden_states[row * hidden_size + idx];
+      int start_id = l * stride + local_id * ElemsPerItem;
+
+      using load_type = sycl::vec<TA, ElemsPerItem>;
+      load_type data;
+      if(start_id < hidden_size){
+        data = *(reinterpret_cast<load_type*>(hidden_states + row * hidden_size + start_id));
+        #pragma unroll
+        for (int i = 0; i < TopK; ++i) {
+          int offset = rows_offset[i];
+          if (offset == -1)
+            continue;
+          *(reinterpret_cast<load_type*>(remapped_hidden_states + offset * hidden_size + start_id)) = data;
         }
       }
     }
 
-    if (hidden_states_scales != nullptr &&
-        remapped_hidden_states_scales != nullptr) {
-      int scaled_hidden_size = hidden_size / block_k;
-      loop_count = (scaled_hidden_size + local_range * ElemsPerItem - 1) /
-                   (local_range * ElemsPerItem);
-      for (int l = 0; l < loop_count; ++l) {
-        int start_id = l * local_range * ElemsPerItem + local_id * ElemsPerItem;
-        int remained_elems = scaled_hidden_size - start_id;
-        if (remained_elems >= ElemsPerItem) {
+//     if (hidden_states_scales != nullptr &&
+//         remapped_hidden_states_scales != nullptr) {
+//       int scaled_hidden_size = hidden_size / block_k;
+//       loop_count = (scaled_hidden_size + local_range * ElemsPerItem - 1) /
+//                    (local_range * ElemsPerItem);
+//       for (int l = 0; l < loop_count; ++l) {
+//         int start_id = l * local_range * ElemsPerItem + local_id * ElemsPerItem;
+//         int remained_elems = scaled_hidden_size - start_id;
+//         if (remained_elems >= ElemsPerItem) {
+// #pragma unroll
+//           for (int i = 0; i < ElemsPerItem; ++i) {
+//             int idx = start_id + i;
+//             remapped_hidden_states_scales
+//                 [rows_offset * scaled_hidden_size + idx] =
+//                     hidden_states_scales[row * scaled_hidden_size + idx];
+//           }
+//         } else if (remained_elems > 0) {
+// #pragma unroll
+//           for (int i = 0; i < remained_elems; ++i) {
+//             int idx = start_id + i;
+//             remapped_hidden_states_scales
+//                 [rows_offset * scaled_hidden_size + idx] =
+//                     hidden_states_scales[row * scaled_hidden_size + idx];
+//           }
+//         }
+//       }
+//     }
+  item.barrier(sycl::access::fence_space::local_space);
+
+  if (local_id == 0) {
 #pragma unroll
-          for (int i = 0; i < ElemsPerItem; ++i) {
-            int idx = start_id + i;
-            remapped_hidden_states_scales
-                [rows_offset * scaled_hidden_size + idx] =
-                    hidden_states_scales[row * scaled_hidden_size + idx];
-          }
-        } else if (remained_elems > 0) {
-#pragma unroll
-          for (int i = 0; i < remained_elems; ++i) {
-            int idx = start_id + i;
-            remapped_hidden_states_scales
-                [rows_offset * scaled_hidden_size + idx] =
-                    hidden_states_scales[row * scaled_hidden_size + idx];
-          }
-        }
+      for(int i = 0; i < TopK; ++i){
+        unpermuted_row_to_permuted_row[row * TopK + i] = rows_offset[i];
       }
     }
   }
