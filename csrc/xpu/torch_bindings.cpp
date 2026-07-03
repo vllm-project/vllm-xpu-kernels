5121b69a3bfd6ff642f55fc3c218706b2441da7a
Zhu, Zufang <zufang.zhu@intel.com>
2026-07-03 06:08:26 +0000

    format
    Signed-off-by: Zhu, Zufang <zufang.zhu@intel.com>

--- csrc/xpu/torch_bindings.cpp
+++ csrc/xpu/torch_bindings.cpp
@@ -40,14 +40,8 @@ TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, xpu_ops) {
 #ifdef VLLM_MOE_ENABLED
   xpu_ops.def(
       "cutlass_grouped_gemm_interface(Tensor ptr_A, Tensor? ptr_A_scale, "
-      "Tensor ptr_B, Tensor? "
-      "ptr_B_scale, "
-      "Tensor? ptr_bias, "
-      "Tensor "
-      "ptr_D, Tensor "
-      "rows_per_expert, int N, int K, int "
-      "num_experts) -> "
-      "Tensor");
+      "Tensor ptr_B, Tensor? ptr_B_scale, Tensor? ptr_bias, Tensor ptr_D, "
+      "Tensor rows_per_expert, int N, int K, int num_experts) -> Tensor");
   xpu_ops.impl(
       "cutlass_grouped_gemm_interface",
       torch::kXPU,
