0ab6d61f2f16adb4633a85be0b6a532c1829a877
mayuyuace <qiming1.zhang@intel.com>
2026-09-08 06:16:12 +0000

    add compact_sampling_mask_kernel
    Signed-off-by: mayuyuace <qiming1.zhang@intel.com>

--- csrc/xpu/torch_bindings.cpp
+++ csrc/xpu/torch_bindings.cpp
@@ -328,6 +328,11 @@ TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, xpu_ops) {
       "fused_input_norm(Tensor! out, Tensor input, Tensor weight, "
       "Tensor bias) -> ()");
   xpu_ops.impl("fused_input_norm", torch::kXPU, &fused_input_norm);
+
+  xpu_ops.def(
+      "compact_sampling_mask(Tensor! logits, Tensor! num_sampled_tokens,"
+      "int max_num_kept, int MAX_COMPACT_SUPPORT) -> (Tensor, Tensor, Tensor)");
+  xpu_ops.impl("compact_sampling_mask", torch::kXPU, &compact_sampling_mask);
 }
 
 REGISTER_EXTENSION(TORCH_EXTENSION_NAME)
