6ee93e38e226814fa89e7f918da4bc36834a5bae
iLeGend <youzhi.jin@intel.com>
2026-09-21 09:31:11 +0800

    revert xe3 modifications
    Signed-off-by: iLeGend <youzhi.jin@intel.com>

--- csrc/xpu/attn/xe_3/fmha_xe3.cpp
+++ csrc/xpu/attn/xe_3/fmha_xe3.cpp
@@ -309,16 +309,6 @@ void cutlass_chunk_prefill_impl(
           is_sink,
           is_lse);
     } else if (args.head_size <= HEAD_SIZE_LIMIT_1) {
-      policy_dispatch_func<chunk_policy_head80_b16>(
-          queue,
-          cuQKType,
-          args,
-          is_paged,
-          is_causal,
-          is_local,
-          is_sink,
-          is_lse);
-    } else if (args.head_size <= HEAD_SIZE_LIMIT_2) {
       policy_dispatch_func<chunk_policy_head96_b16>(
           queue,
           cuQKType,
@@ -328,7 +318,7 @@ void cutlass_chunk_prefill_impl(
           is_local,
           is_sink,
           is_lse);
-    } else if (args.head_size <= HEAD_SIZE_LIMIT_3) {
+    } else if (args.head_size <= HEAD_SIZE_LIMIT_2) {
       policy_dispatch_func<chunk_policy_head128_b16>(
           queue,
           cuQKType,
@@ -338,7 +328,7 @@ void cutlass_chunk_prefill_impl(
           is_local,
           is_sink,
           is_lse);
-    } else if (args.head_size <= HEAD_SIZE_LIMIT_4) {
+    } else if (args.head_size <= HEAD_SIZE_LIMIT_3) {
       policy_dispatch_func<chunk_policy_head192_b16>(
           queue,
           cuQKType,
@@ -348,7 +338,7 @@ void cutlass_chunk_prefill_impl(
           is_local,
           is_sink,
           is_lse);
-    } else if (args.head_size <= HEAD_SIZE_LIMIT_5) {
+    } else if (args.head_size <= HEAD_SIZE_LIMIT_4) {
       policy_dispatch_func<chunk_policy_head256_b16>(
           queue,
           cuQKType,
@@ -358,7 +348,7 @@ void cutlass_chunk_prefill_impl(
           is_local,
           is_sink,
           is_lse);
-    } else if (args.head_size <= HEAD_SIZE_LIMIT_6) {
+    } else if (args.head_size <= HEAD_SIZE_LIMIT_5) {
       policy_dispatch_func<chunk_policy_head512_b16>(
           queue,
           cuQKType,
@@ -375,21 +365,18 @@ void cutlass_chunk_prefill_impl(
     policy_dispatch_func<chunk_policy_head64>(
         queue, cuQKType, args, is_paged, is_causal, is_local, is_sink, is_lse);
   } else if (args.head_size <= HEAD_SIZE_LIMIT_1) {
-    policy_dispatch_func<chunk_policy_head80>(
-        queue, cuQKType, args, is_paged, is_causal, is_local, is_sink, is_lse);
-  } else if (args.head_size <= HEAD_SIZE_LIMIT_2) {
     policy_dispatch_func<chunk_policy_head96>(
         queue, cuQKType, args, is_paged, is_causal, is_local, is_sink, is_lse);
-  } else if (args.head_size <= HEAD_SIZE_LIMIT_3) {
+  } else if (args.head_size <= HEAD_SIZE_LIMIT_2) {
     policy_dispatch_func<chunk_policy_head128>(
         queue, cuQKType, args, is_paged, is_causal, is_local, is_sink, is_lse);
-  } else if (args.head_size <= HEAD_SIZE_LIMIT_4) {
+  } else if (args.head_size <= HEAD_SIZE_LIMIT_3) {
     policy_dispatch_func<chunk_policy_head192>(
         queue, cuQKType, args, is_paged, is_causal, is_local, is_sink, is_lse);
-  } else if (args.head_size <= HEAD_SIZE_LIMIT_5) {
+  } else if (args.head_size <= HEAD_SIZE_LIMIT_4) {
     policy_dispatch_func<chunk_policy_head256>(
         queue, cuQKType, args, is_paged, is_causal, is_local, is_sink, is_lse);
-  } else if (args.head_size <= HEAD_SIZE_LIMIT_6) {
+  } else if (args.head_size <= HEAD_SIZE_LIMIT_5) {
     policy_dispatch_func<chunk_policy_head512>(
         queue, cuQKType, args, is_paged, is_causal, is_local, is_sink, is_lse);
   } else {
